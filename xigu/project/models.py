db98e70ac0464fd6a48d4e3d34f07bec0008d539
skygon <skygoncui@163.com>
2018-08-08 13:27:58 +0800

    create model

--- xigu/project/models.py
+++ xigu/project/models.py
@@ -30,13 +30,29 @@ class ProjectDescription(models.Model):
 
 class Project(models.Model):
     TYPE = ((1, '基金'),(2, '保险'),(3, '商业地产'))
+    STATUS = ((1, '募集中'), (2, '已结束'))
+    SHOW = ((0, '否'),(1, '是'))
 
     project_name = models.CharField(max_length=128, verbose_name="产品名称")
     project_type = models.IntegerField(choise=TYPE, verbose_name="产品类型")
     estimate_yearly_return = models.CharField(max_length=32, verbose_name="预计年化收益率")
-    invest_range = models.CharField(max_length=64, verbose_name="投资期限")
+    history_yearly_return = models.CharField(max_length=32, verbose_name="历史平均年化收益率", null=True, blank=True)
 
+    project_status = models.IntegerField(default=1, choise=STATUS, verbose_name="产品状态")
     # 基金类产品字段
+    min_amount = models.CharField(max_length=32, verbose_name="起投金额", null=True, blank=True)
+    step_amount = models.CharField(max_length=32, verbose_name="递增金额", null=True, blank=True)
+    # some projects may not have invest range property.
+    invest_range = models.CharField(max_length=64, verbose_name="投资期限", null=True, blank=True)
+
+    project_detail = models.OneToOneField(ProjectDescription, on_delete=models.CASCADE, null=True, blank=True, verbose_name="产品详细描述")
+
+    #是否需要在项目列表中显示
+    is_show = models.IntegerField(choices=SHOW, default=1, verbose_name="是显示该产品")
+
+    # 置顶
+    is_top = models.IntegerField(default=0, verbose_name="是否置顶", help_text="设置大于0的数表示置顶，数字越大优先级越高")
+    
     class Meta:
         db_table = "project"
         verbose_name = '产品'
